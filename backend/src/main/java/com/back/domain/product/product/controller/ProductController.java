65744a3e849a1b9b221f0dc427f2de71175cb972
GerHerMo <drugbeggar@gmail.com>
2025-07-15 18:01:01 +0900

    step 04 - v1

---
+++ backend/src/main/java/com/back/domain/product/product/controller/ProductController.java
@@ -0,0 +1,56 @@
+package com.back.domain.product.product.controller;
+
+import com.back.domain.product.product.entity.Product;
+import com.back.domain.product.product.service.ProductService;
+import com.back.global.rsdata.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+@Tag(name =  "PostController", description = "API 상품 컨트롤러")
+public class ProductController {
+    private final ProductService productService;
+
+    record ProductModifyReqbody(
+
+            @NotBlank @Size(min = 1, max = 100)
+            String name,
+
+            @NotBlank
+            String imageUrl,
+
+            @NotBlank
+            String info,
+
+            @NotBlank
+            Integer price,
+
+            String engName
+    ) {
+    }
+
+
+    @PutMapping("/{id}")
+    @Transactional
+    @Operation(summary = "상품 수정")
+    public RsData<Void> updateProduct(
+            @PathVariable int id,
+            @Valid @RequestBody ProductModifyReqbody reqBody
+    ) {
+        Product product = productService.findById(id).get();
+        productService.modify(product, reqBody.name, reqBody.imageUrl, reqBody.info, reqBody.price, reqBody.engName);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 상품이 수정되었습니다.".formatted(product.getId())
+        );
+    }
+}
--- backend/src/main/java/com/back/domain/product/product/service/ProductService.java
+++ backend/src/main/java/com/back/domain/product/product/service/ProductService.java
@@ -1,4 +1,26 @@
 package com.back.domain.product.product.service;
 
+import com.back.domain.product.product.entity.Product;
+import com.back.domain.product.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public long count() {
+        return productRepository.count();
+    }
+
+    public Optional<Product> findById(int id) {
+        return productRepository.findById(id);
+    }
+
+    public void modify(Product product, String name, String imageUrl, String info, int price, String engName) {
+        product.modify(name, imageUrl, info, price, engName);
+    }
 }
