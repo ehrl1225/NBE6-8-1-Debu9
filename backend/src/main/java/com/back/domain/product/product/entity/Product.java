78cec0f7d8a2a12407e5f0c08416f07e72d3e04a
GerHerMo <drugbeggar@gmail.com>
2025-07-15 14:38:37 +0900

    step 01

---
+++ backend/src/main/java/com/back/domain/product/product/entity/Product.java
@@ -0,0 +1,11 @@
+package com.back.domain.product.product.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Product {
+}
