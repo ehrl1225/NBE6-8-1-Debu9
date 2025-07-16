7a56639d0c27a0dc42e944f2bf8fb12b8e9c73ae
taekyung <taekyung630@naver.com>
2025-07-16 17:26:36 +0900

    장바구니 목록과 구매정보 컴포넌트 분리

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,84 +1,100 @@
-export default function page() {
+"use client";
+import { Product } from "../type/product";
+import { dummyCartItems } from "../data/dummydata";
+
+const total_price = dummyCartItems.reduce(
+  (sum, item) => sum + item.product.price * item.quantity,
+  0
+);
+
+const total_item = dummyCartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+const ShoppingCart = () => {
   return (
     <>
-      <div className="flex h-screen bg-[#F9F9F2]">
-        <div className="w-[60%] h-full">
-          <div className="bg-white m-20 p-8">
-            <div className="flex items-center">
-              <input type="checkbox" className="w-4 h-4" />
-              <p className="text-sm ml-2">전체 선택</p>
-              <button className="text-xs ml-100 cursor-pointer border border-gray-300 px-3 py-1 rounded-xl">
-                선택 삭제
-              </button>
-            </div>
-            <ul className="flex flex-col">
-              <div className="flex border-b border-b-gray mx-5">
-                <input type="checkbox" />
-                <img src="/images/starbu9_blend1.png"></img>
-                <div className="mt-6">
-                  <p className="text-sm font-bold">스타버구 블렌드1</p>
-                  <p className="text-sm font-thin">StarBu9 Blend1</p>
-                  <br />
-                  <p className="text-xs">1개/20,000원</p>
-                  <button className="cursor-pointer border border-gray-300 text-xs py-1 px-3 rounded-xl">
-                    수량 변경
-                  </button>
-                </div>
-              </div>
+      <div className="bg-white">
+        <input type="checkbox" />
+        <span className="text-sm">전체 선택</span>
+        <button className="cursor-pointer border border-gray-300 text-sm py-1 px-3 rounded-xl">
+          선택 삭제
+        </button>
+        <ul>
+          {dummyCartItems.map((item) => {
+            return (
+              <>
+                <li key={item.product.id} className="flex gap-10">
+                  <input type="checkbox" />
+                  <img src={item.product.imageUrl} />
+                  <div className="mt-6">
+                    <p className="text-sm font-bold">{item.product.name}</p>
+                    <p className="text-sm font-thin">{item.product.engName}</p>
+                    <br />
+                    <p className="text-xs">
+                      {item.quantity}개/{item.product.price * item.quantity}원
+                    </p>
+                    <button className="cursor-pointer border border-gray-300 text-sm py-1 px-3 rounded-xl">
+                      수량 수정
+                    </button>
+                  </div>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
 
-              <div className="flex mx-5">
-                <input type="checkbox" />
-                <img src="/images/starbu9_blend2.png"></img>
-                <div className="mt-6">
-                  <p className="text-sm font-bold">스타버구 블렌드2</p>
-                  <p className="text-sm font-thin">StarBu9 Blend2</p>
-                  <br />
-                  <p className="text-xs">2개/40,000원</p>
-                  <button className="cursor-pointer border border-gray-300 text-xs  py-1 px-3 rounded-xl">
-                    수량 변경
-                  </button>
-                </div>
-              </div>
-            </ul>
-          </div>
-        </div>
-
-        <div className="w-[40%] left-[60%] px-10 flex flex-col items-center border-l border-l-gray-300">
-          <div className="w-full flex flex-col items-start gap-8 border-b border-b-gray-300 pb-8 mt-10">
-            <p className="text-2xl">구매 금액</p>
-            <div className="flex gap-50">
-              <div className="flex flex-col gap-3">
-                <p>상품 금액</p>
-                <p>배송비</p>
-              </div>
-              <div className="flex flex-col gap-3 items-end">
-                <p>60,000원</p>
-                <p>무료배송</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-full flex flex-col items-start mt-10">
+const Order = () => {
+  return (
+    <>
+      <div className="fix4w-[40%] left-[60%] px-10 flex flex-col items-center border-l border-l-gray-300">
+        <div className="w-full flex flex-col items-start gap-8 border-b border-b-gray-300 pb-8 mt-10">
+          <p className="text-2xl">구매 금액</p>
+          <div className="flex gap-50">
             <div className="flex flex-col gap-3">
-              <p className="text-2xl pb-8">구매자 정보</p>
-              <label>이메일</label>
-              <input
-                placeholder="email"
-                className="border border-gray-300 pl-10 p-2 rounded-lg 
-              bg-white bg-[url('/images/email.png')] bg-no-repeat bg-[length:18px_18px] bg-[position:10px_center]"
-              />
-              <label>주소</label>
-              <input
-                placeholder="address"
-                className="border border-gray-300 pl-10 p-2 rounded-lg
-              bg-white bg-[url('/images/address.png')] bg-no-repeat bg-[length:18px_18px] bg-[position:10px_center]"
-              />
-              <button className="p-2 mt-8 px-5 bg-[#005034] text-white rounded-xl">
-                60,000원 결제하기(3개)
-              </button>
+              <p>상품 금액</p>
+              <p>배송비</p>
+            </div>
+            <div className="flex flex-col gap-3 items-end">
+              <p>{total_price}</p>
+              <p>무료배송</p>
             </div>
           </div>
         </div>
+
+        <div className="w-full flex flex-col items-start mt-10">
+          <div className="flex flex-col gap-3">
+            <p className="text-2xl pb-8">구매자 정보</p>
+            <label>이메일</label>
+            <input
+              placeholder="email"
+              className="border border-gray-300 pl-10 p-2 rounded-lg 
+              bg-white bg-[url('/images/email.png')] bg-no-repeat bg-[length:18px_18px] bg-[position:10px_center]"
+            />
+            <label>주소</label>
+            <input
+              placeholder="address"
+              className="border border-gray-300 pl-10 p-2 rounded-lg
+              bg-white bg-[url('/images/address.png')] bg-no-repeat bg-[length:18px_18px] bg-[position:10px_center]"
+            />
+            <button className="p-2 mt-8 px-5 bg-[#005034] text-white rounded-xl">
+              {total_price}원 결제하기({total_item}개)
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default function Page() {
+  return (
+    <>
+      <div className="h-full bg-gray-300 flex">
+        <ShoppingCart />
+        <Order />
       </div>
     </>
   );
