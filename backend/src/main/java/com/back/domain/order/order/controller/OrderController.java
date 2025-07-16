78a832ebaee03bee7f2531c82b3724e39195e2c0
GerHerMo <drugbeggar@gmail.com>
2025-07-16 15:23:48 +0900

    step 01 - base

--- backend/src/main/java/com/back/domain/order/order/controller/OrderController.java
+++ backend/src/main/java/com/back/domain/order/order/controller/OrderController.java
@@ -1,10 +1,17 @@
 package com.back.domain.order.order.controller;
 
+import com.back.domain.order.order.dto.OrderDto;
+import com.back.domain.order.order.entity.Order;
 import com.back.domain.order.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -12,4 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "OrderController", description = "API 주문 컨트롤러")
 public class OrderController {
     private final OrderService orderService;
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    @Operation(summary = "주문 다건 조회")
+    public List<OrderDto> getItems() {
+        List<Order> items = orderService.findAll();
+
+        return items
+                .stream()
+                .map(OrderDto::new) // OrderDto로 변환
+                .toList();
+    }
+
 }
--- backend/src/main/java/com/back/domain/order/order/service/OrderService.java
+++ backend/src/main/java/com/back/domain/order/order/service/OrderService.java
@@ -1,9 +1,22 @@
 package com.back.domain.order.order.service;
 
+import com.back.domain.order.order.entity.Order;
+import com.back.domain.order.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public long count() {
+        return orderRepository.count();
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
 }
