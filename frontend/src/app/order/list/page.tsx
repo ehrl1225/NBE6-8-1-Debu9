bff49869a28127e34cb7cc7e79c7739fe0b45a8e
ehrl1225 <ehrl1225@github.com>
2025-07-18 15:15:47 +0900

    서버로부터 주문을 조회

--- frontend/src/app/order/list/page.tsx
+++ frontend/src/app/order/list/page.tsx
@@ -1,6 +1,10 @@
 "use client"
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, FormEventHandler, FormEvent } from 'react';
 import Link from "next/link";
+import type {components} from "@/lib/backend/apiV1/schema"
+
+type OrderResponseDto = components["schemas"]["OrderResponseDto"]
+type OrderItemResponseDto = components["schemas"]["OrderItemResponseDto"]
 
 interface OrderItem{
     order_item_num:number,
@@ -21,7 +25,7 @@ interface Order{
     order_items:OrderItem[]
 }
 
-export function OrderItem({orderItem}:{orderItem:OrderItem}){
+export function OrderItem({orderItem}:{orderItem:OrderItemResponseDto}){
     return (
         <div className='border my-2 rounded-xl flex border-gray-300'>
             <div className='p-2'>
@@ -36,7 +40,7 @@ export function OrderItem({orderItem}:{orderItem:OrderItem}){
                             <a>{orderItem.product_eng_name}</a>
                         </div>
                         <div className='mt-2 flex'>
-                            <a className='block'>{orderItem.price.toLocaleString("ko-KR")} 원</a>
+                            <a className='block'>{orderItem.price!.toLocaleString("ko-KR")} 원</a>
                             <a className='block ml-1'>{orderItem.count} 개</a>
                         </div>
                     </div>
@@ -45,7 +49,7 @@ export function OrderItem({orderItem}:{orderItem:OrderItem}){
 
             <div className='border-l border-l-gray-300 ml-3 px-2'>
                 <div className='border rounded my-2 border-gray-300'>
-                    <Link href={`/order/delivery/${orderItem.order_item_num}`} className='text-center block m-1 p-1'>배송 조회</Link>
+                    <Link href={`/order/delivery/${orderItem.orderItem_id}`} className='text-center block m-1 p-1'>배송 조회</Link>
                 </div>
                 <div className='border rounded my-2 border-gray-300'>
                     <a className='text-center block m-1 p-1'>교환, 반품 신청</a>
@@ -58,106 +62,50 @@ export function OrderItem({orderItem}:{orderItem:OrderItem}){
     )
 }
 
-function OrderList({data}:{data:Order}){
+function OrderList({data}:{data:OrderResponseDto}){
     return (
         <div className='p-3 rounded-xl my-3 shadow-[0_0_5px_rgba(0,0,0,0.3)]'>
             <span className='flex justify-between'>
                 <a className='block text-xl font-bold'>{data.created_date} 주문</a>
                 <Link className='text-blue-500 block' href={`/order/${data.order_number}`}>주문 상세 보기&gt;</Link>
             </span>
-            {data.order_items.map((e)=><OrderItem orderItem={e} key={e.order_item_num}></OrderItem>)}
+            {data.order_items!.map((e)=><OrderItem orderItem={e} key={e.orderItem_id}></OrderItem>)}
 
         </div>
     )
 }
 
 export default function page() {
-    const datas:Order[] = [
-        {
-            order_number:'111',
-            total_price: 1000,
-            order_address: "강남 프로그래머스",
-            total_count:1,
-            created_date:"2025. 07. 16",
-            order_items:[
-                {
-                    order_item_num: 1,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드1",
-                    product_eng_name: "Starbu9 Blend1",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend1.png",
-                    count:1
-                  },
-                  {
-                    order_item_num: 2,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드2",
-                    product_eng_name: "Starbu9 Blend2",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend2.png",
-                    count:1
-                  },
-                  {
-                    order_item_num: 3,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드3",
-                    product_eng_name: "Starbu9 Blend3",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend3.png",
-                    count:1
-                  },
-                  {
-                    order_item_num: 4,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드4",
-                    product_eng_name: "Starbu9 Blend4",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend4.png",
-                    count:1
-                  },
-            ]
-        },
-        {
-            order_number:'1233',
-            total_price: 4000,
-            order_address: "내 집",
-            total_count: 3,
-            created_date:"2025. 07. 15",
-            order_items:[
-                {
-                    order_item_num: 1,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드1",
-                    product_eng_name: "Starbu9 Blend1",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend1.png",
-                    count:1
-                  },
-                  {
-                    order_item_num: 2,
-                    delivery_state: "배송중",
-                    product_name: "스타버구 블렌드2",
-                    product_eng_name: "Starbu9 Blend2",
-                    price: 20000,
-                    image_url: "/images/starbu9_blend2.png",
-                    count:1
-                  },
-            ]
+    const [memberEmail, setMemberEmail] = useState("");
+    const [orders, setOrders] = useState<OrderResponseDto[]>([]);
+
+    const onSubmit = async (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        try{
+            // const 
+            const response = await fetch(`http://localhost:8080/api/orders/search?memberEmail=${memberEmail}`)
+            const data:OrderResponseDto[] = await response.json();
+            setOrders(data);
+        }catch(error){
+
         }
-    ];
+    }
+
     return (
         <div className='mt-10 text-black'>
             <div className='flex justify-center'>
-                <form>
-                    <input type='text' className='border p-2' placeholder='이메일 주소를 작성해 주세요.'></input>
-                    <button className='border ml-3 rounded p-2'>조회</button>
+                <form onSubmit={onSubmit}>
+                    <input type='email' value={memberEmail} onChange={(e)=>setMemberEmail(e.target.value)} className='border p-2' placeholder='이메일 주소를 작성해 주세요.'></input>
+                    <button className='border ml-3 rounded p-2 cursor-pointer' type='submit'>조회</button>
                 </form>
             </div>
             <div className='flex justify-center mt-3'>
+                {orders.length==0?
+                <p>조회된 주문이 없습니다.</p> :
                 <ul>
-                    {datas.map((e)=><OrderList data={e} key={e.order_number}></OrderList>)}
-                </ul>
+                    {orders.map((e)=><OrderList data={e} key={e.order_number}></OrderList>)}
+                </ul>}
+                
             </div>
         </div>
     ); 
