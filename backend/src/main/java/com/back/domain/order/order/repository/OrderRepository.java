3c19ed3fae1c5be22a3eac4b16f7b08824a90b34
GerHerMo <drugbeggar@gmail.com>
2025-07-18 12:30:23 +0900

    06 - OrderRepository modified

--- backend/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
+++ backend/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
@@ -3,12 +3,17 @@ package com.back.domain.order.order.repository;
 
 import com.back.domain.order.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    @Query("SELECT o FROM Order o JOIN FETCH o.items oi JOIN FETCH oi.product p WHERE o.user.email = :email")
+    List<Order> findAllByMemberEmailWithItemsAndProducts(String email);
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.items oi JOIN FETCH oi.product p")
+    List<Order> findAllWithItemsAndProducts();
 }
 
